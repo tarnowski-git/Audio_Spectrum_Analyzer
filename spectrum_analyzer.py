960336eb45d565f0c7398b1b18b330ee8db63a62
Konrad <konrad.tarnowski@o2.pl>
2019-11-09 23:26:28 +0100

    Add own package
    This change is for making the project more SOLID.

---
+++ spectrum_analyzer.py
@@ -0,0 +1,4 @@
+from spectrum_analyzer.main import main
+
+if __name__ == '__main__':
+    main()
---
+++ spectrum_analyzer/main.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+
+
+class MainApplication(tk.Frame):
+
+    def __init__(self, master):
+        self.master = master
+        tk.Frame.__init__(self, self.master)
+        self._configure_gui()
+        self._create_widgets()
+
+    def _configure_gui(self):
+        """Setting general configurations of the application."""
+        self.configure(background="white")
+
+        self.master.title("Snake game")
+        self.master.geometry("500x500")
+        self.master.resizable(False, False)
+
+
+
+    def _create_widgets(self):
+        """Creating the widgets of the application."""
+        
+        pass
+
+
+def main():
+    """Function to run mianloop"""
+    root = tk.Tk()
+    main_app =  MainApplication(root)
+    root.mainloop()
+
+
+# if __name__ == '__main__':
+#     main()
